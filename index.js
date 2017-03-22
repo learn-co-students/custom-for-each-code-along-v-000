adfce60b418e55a1aed9de50584979bf6d3925ba
Jennifer Pazos <jmp521@gmail.com>
2017-03-21 22:33:18 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+function forEach(iterable, callback) {
+  if(Array.isArray(iterable)) {
+    for (let i=0, l = iterable.length; i < l; i++) {
+      const element = iterable[i];
+      callback(element, i, iterable);
+    }
+  } else if (typeof iterable === 'object') {
+    for (const key in iterable) {
+      callback(value, key, iterable)
+    }
+  }
+}
