42535b44f95c3fbf7363689bc852245bb4d85201
WilliamBarela <william.barela.dev@gmail.com>
2017-07-19 12:04:10 +0900

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,30 @@ function forEach(iterable, callback) {
     }
   }
 }
+
+// How to use this glorious piece of code:
+
+/*
+
+  To use this function, you must define a callback function which works for the iterable you are working on.
+  Here is some example data:
+
+    const dogs = [
+      { name: "Fido", age: 2 },
+      { name: "Odie", age: 8 },
+      { name: "Ralph", age: 4 }
+    ];
+
+  Let say we want to go through each of these and print out that "Fido is a 2 years old." etc., then we can make a callback:
+
+    function tellAge(value, key, iterable){
+      console.log(`${value.name} is ${value.age} years old.`);
+    }
+   
+  And then we can run the forEach function on it like so:
+
+    forEach(dogs, tellAge)
+
+  As you can see, iterable refers to the array dogs, key refers to which iteration javascript has gone through, and value refers to the value of iterable[key].
+
+*/
